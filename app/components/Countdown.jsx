fef872d938c02856ed3c120c81bc5953cf263945
jobvink <jmcvink@gmail>
2017-05-29 16:27:20 +0200

    timer gemaakt

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -69,6 +69,7 @@ var Countdown = React.createClass({
 
 		return (
 			<div>
+				<h1 className="page-title">Countdown app</h1>
 				<Clock totalSeconds={count}/>
 				{renderControlArea()}
 			</div>
